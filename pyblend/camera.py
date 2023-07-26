dcefd742ba9583d3a8a862b5f3f6e88ea57d4900
anyeZHY <47559723+anyeZHY@users.noreply.github.com>
2023-07-26 15:31:46 -0400

    add functions for camera and utils

--- pyblend/camera.py
+++ pyblend/camera.py
@@ -1,9 +1,22 @@
+import bpy
 import numpy as np
+from mathutils import Matrix
 
 
-def get_K_intr_from_blender(focal_length, width=640, height=480):
+def get_K_intr_from_blender(camera: bpy.types.Camera = None, width=None, height=None):
+    """
+    Get the intrinsic matrix from Blender.
+    """
+    if width is None:
+        width = bpy.context.scene.render.resolution_x
+    if height is None:
+        height = bpy.context.scene.render.resolution_y
+    if camera is None:
+        angle = bpy.data.cameras["Camera"].angle
+    else:
+        angle = camera.angle
+
     aspect_ratio = width / height
-    angle = 2 * np.arctan(18 / focal_length)
     K = np.zeros((3, 3), dtype=np.float32)
     K[0][0] = width / 2 / np.tan(angle / 2)
     K[1][1] = height / 2.0 / np.tan(angle / 2) * aspect_ratio
@@ -11,4 +24,51 @@ def get_K_intr_from_blender(focal_length, width=640, height=480):
     K[1][2] = height / 2.0
     K[2][2] = 1.0
     K.transpose()
-    return K
+    return K.astype(np.float32)
+
+
+def get_3x4_RT_matrix_from_blender(camera: bpy.types.Camera = None):
+    if camera is None:
+        camera = bpy.data.objects["Camera"]
+    R_bcam2cv = Matrix(((1, 0, 0), (0, -1, 0), (0, 0, -1)))
+
+    location, rotation = camera.matrix_world.decompose()[0:2]
+    R_world2bcam = rotation.to_matrix().transposed()
+
+    T_world2bcam = -1 * R_world2bcam @ location
+
+    R_world2cv = R_bcam2cv @ R_world2bcam
+    T_world2cv = R_bcam2cv @ T_world2bcam
+
+    RT = Matrix(
+        (
+            R_world2cv[0][:] + (T_world2cv[0],),
+            R_world2cv[1][:] + (T_world2cv[1],),
+            R_world2cv[2][:] + (T_world2cv[2],),
+            [0, 0, 0, 1],
+        )
+    )
+    return np.array(RT).astype(np.float32)
+
+
+def get_camera_para(camera=None):
+    """
+    Get the camera parameters from Blender.
+    Reference: https://blender.stackexchange.com/questions/38009/3x4-camera-matrix-from-blender-camera
+
+    Args:
+        camera (bpy.types.Object, optional): camera object. Defaults to None.
+
+    Returns:
+        dict: camera parameters, including intrinsic and extrinsic matrices
+    """
+    if camera is None:
+        camera = bpy.data.objects["Camera"]
+    K = get_K_intr_from_blender(camera)
+    T = get_3x4_RT_matrix_from_blender(camera)
+
+    meta = {
+        "intrinsic": K,
+        "extrinsic": np.array(T),
+    }
+    return meta
--- pyblend/utils.py
+++ pyblend/utils.py
@@ -1,19 +1,67 @@
 import os
 import bpy
+import sys
+import argparse
+from typing import List
 
 
-def clear():
-    for obj in bpy.data.objects:
-        bpy.data.objects.remove(obj)
-    for m in bpy.data.meshes:
-        bpy.data.meshes.remove(m)
-    for mat in bpy.data.materials:
-        bpy.data.materials.remove(mat)
-    for img in bpy.data.images:
-        bpy.data.images.remove(img)
+class ArgumentParserForBlender(argparse.ArgumentParser):
+    """
+    Reference: https://blender.stackexchange.com/a/134596
+    This class is identical to its superclass, except for the parse_args
+    method (see docstring). It resolves the ambiguity generated when calling
+    Blender from the CLI with a python script, and both Blender and the script
+    have arguments. E.g., the following call will make Blender crash because
+    it will try to process the script's -a and -b flags:
+    >>> blender --python my_script.py -a 1 -b 2
+
+    To bypass this issue this class uses the fact that Blender will ignore all
+    arguments given after a double-dash ('--'). The approach is that all
+    arguments before '--' go to Blender, arguments after go to the script.
+    The following calls work fine:
+    >>> blender --python my_script.py -- -a 1 -b 2
+    >>> blender --python my_script.py --
+    """
+
+    def _get_argv_after_doubledash(self):
+        """
+        Given the sys.argv as a list of strings, this method returns the
+        sublist right after the '--' element (if present, otherwise returns
+        an empty list).
+        """
+        try:
+            idx = sys.argv.index("--")
+            return sys.argv[idx + 1 :]  # the list after '--'
+        except ValueError as e:  # '--' not in the list:
+            return []
+
+    # overrides superclass
+    def parse_args(self):
+        """
+        This method is expected to behave identically as in the superclass,
+        except that the sys.argv list will be pre-processed using
+        _get_argv_after_doubledash before. See the docstring of the class for
+        usage examples and details.
+        """
+        return super().parse_args(args=self._get_argv_after_doubledash())
 
 
 class BlenderRemover:
+    """
+    This class is used to remove objects, meshes, materials, and images
+    created by Blender. It is used to remove temporary objects created
+    during the rendering process.
+
+    To remove objects, meshes, materials, and images, simply call
+    >>> remover = BlenderRemover()  # create a remover
+    >>> remover.add(obj, img)  # add objects to be removed
+    >>> remover()  # remove objects
+
+    If you want to remove all objects, meshes, materials, and images
+    created by Blender, simply call
+    >>> remover.clear_all()
+    """
+
     def __init__(self):
         self.mats = []
         self.objs = []
@@ -23,7 +71,8 @@ class BlenderRemover:
     def add(self, obj: bpy.types.Object = None, img: bpy.types.Image = None):
         if obj is not None:
             self.objs.append(obj)
-            self.mats.append(obj.active_material)
+            if obj.active_material is not None:
+                self.mats.append(obj.active_material)
             self.meshes.append(obj.data)
         if img is not None:
             self.imgs.append(img)
@@ -40,9 +89,41 @@ class BlenderRemover:
             if bpy.data.images.get(img_name) is not None:
                 bpy.data.images.remove(bpy.data.images[img_name])
 
+        self.reset()
+
+    def reset(self):
+        self.mats = []
+        self.objs = []
+        self.meshes = []
+        self.imgs = []
+
+    def clear_all(
+        self,
+        exclude: List(str or bpy.types.Object or bpy.types.Image or bpy.types.Mesh or bpy.types.Material) = [],
+    ):
+        """
+        Clear all objects, meshes, materials, and images created by Blender
+
+        Args:
+            exclude (List): objects, meshes, materials, and images to be excluded
+        """
+        for mat in bpy.data.materials:
+            if mat not in exclude or mat.name not in exclude:
+                bpy.data.materials.remove(mat)
+        for obj in bpy.data.objects:
+            if obj not in exclude or obj.name not in exclude:
+                bpy.data.objects.remove(obj)
+        for mesh in bpy.data.meshes:
+            if mesh not in exclude or mesh.name not in exclude:
+                bpy.data.meshes.remove(mesh)
+        for img in bpy.data.images:
+            if img not in exclude or img.name not in exclude:
+                bpy.data.images.remove(img)
+
 
 def debug(file_path="debug.blend"):
-    """Save the current scene to a blend file for debugging
+    """
+    Save the current scene to a blend file for debugging
 
     Args:
         file_path (str, optional): path to save the blend file. Defaults to "debug.blend".
